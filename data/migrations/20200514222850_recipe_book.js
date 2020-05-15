187fbde66dd6ef4b77c86aaa20f17a2fbe510060
Gordon Caister <gordoncaister@gmail.com>
2020-05-14 22:52:27 -0500

    finished migration up, working on down

--- data/migrations/20200514222850_recipe_book.js
+++ data/migrations/20200514222850_recipe_book.js
@@ -7,18 +7,30 @@ exports.up = function(knex) {
     })
     .createTable("steps",steps=>{
         steps.increments();
-        steps.integer("number").notNullable();
+        steps.integer("number").unsigned().notNullable();
         steps.text("description").notNullable();
+        steps.integer("recipe").unsigned().notNullable()
+            .references("recipes.id")
+            .onUpdate("CASCADE")
+            .onDelete("DO NOTHING");
     })
     .createTable("ingredients",ingredients=>{
         ingredients.increments();
         ingredients.string("name", 255).notNullable();
         
     })
-    .createTable("recipes",recipes=>{
-        recipes.increments();
-        recipes.string("unit_of_measurement", 100).notNullable();
-        ingredients.float("quanitity").notNullable();
+    .createTable("recipes_ingredients",recipesIngredients=>{
+        recipesIngredients.increments();
+        recipesIngredients.string("unit_of_measurement", 100).notNullable();
+        recipesIngredients.float("quanitity").notNullable();
+        recipesIngredients.integer("recipe").unsigned().notNullable()
+            .references("recipes.id")
+            .onUpdate("CASCADE")
+            .onDelete("DO NOTHING");
+        recipesIngredients.integer("ingredients").unsigned().notNullable()
+            .references("ingredients.id")
+            .onUpdate("CASCADE")
+            .onDelete("DO NOTHING");
     })
   
 };
